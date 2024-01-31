b4523a7bb6ef3a7771b6bb710c6b31ab3db61520
ThorsteinA <155554708+ThorsteinA@users.noreply.github.com>
2024-01-31 10:34:08 +0100

    Add files via upload

--- Chapter5/portofolio/coolEffects.js
+++ Chapter5/portofolio/coolEffects.js
@@ -1,86 +1,85 @@
 function isLandscapeMode() {
-    return window.matchMedia("(orientation: landscape)").matches;
-  }
+  return window.matchMedia("(orientation: landscape)").matches;
+}
 
 //scroll bar
 
 let progress = document.getElementById("progressbar");
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 
-window.onscroll = function(){
-    if (isLandscapeMode()) {
+window.onscroll = function () {
+  if (isLandscapeMode()) {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + "%";
-}
-}
+  }
+};
 
 //text på main side
 
 let textMainSide = document.querySelector(".sec-text");
 
 function getRandomInt(min, max) {
-    if (isLandscapeMode()) {
+  if (isLandscapeMode()) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+
+function textLoad() {
+  if (isLandscapeMode()) {
+    let numberForText = getRandomInt(1, 12);
+    if (numberForText === 1) {
+      textMainSide.textContent = "Thorstein Andreassen";
+    }
+    if (numberForText === 2) {
+      textMainSide.textContent = "en elev";
+    }
+    if (numberForText === 3) {
+      textMainSide.textContent = "en programmerer";
+    }
+    if (numberForText === 4) {
+      textMainSide.textContent = "interessert i å lære";
+    }
+
+    if (numberForText === 5) {
+      textMainSide.textContent = "en fitnessentusiast";
+    }
+
+    if (numberForText === 6) {
+      textMainSide.textContent = "en kodekunstner";
+    }
+
+    if (numberForText === 7) {
+      textMainSide.textContent = "en idrettsstudent";
+    }
+
+    if (numberForText === 8) {
+      textMainSide.textContent = "en programmeringsnerd";
+    }
+
+    if (numberForText === 9) {
+      textMainSide.textContent = "en kodeskaper";
+    }
+
+    if (numberForText === 10) {
+      textMainSide.textContent = "en skoleelsker";
+    }
+
+    if (numberForText === 11) {
+      textMainSide.textContent = "en kodeutvikler";
+    }
+
+    if (numberForText === 12) {
+      textMainSide.textContent = "i skolemodus";
     }
   }
-
-function textLoad(){
-    if (isLandscapeMode()) {
-    let numberForText = getRandomInt(1, 12);
-    if (numberForText === 1){
-        textMainSide.textContent = "Thorstein Andreassen"
-    }
-    if (numberForText === 2){
-        textMainSide.textContent = "en elev"
-    }
-    if (numberForText === 3){
-        textMainSide.textContent = "en programmerer"
-    }
-    if (numberForText === 4){
-        textMainSide.textContent = "interessert i å lære"
-    }
-
-    if (numberForText === 5){
-        textMainSide.textContent = "en fitnessentusiast"
-    }
-
-    if (numberForText === 6){
-        textMainSide.textContent = "en kodekunstner"
-    }
-
-    if (numberForText === 7){
-        textMainSide.textContent = "en idrettsstudent"
-    }
-
-    if (numberForText === 8){
-        textMainSide.textContent = "en programmeringsnerd"
-    }
-
-    if (numberForText === 9){
-        textMainSide.textContent = "en kodeskaper"
-    }
-
-    if (numberForText === 10){
-        textMainSide.textContent = "en skoleelsker"
-    }
-
-    if (numberForText === 11){
-        textMainSide.textContent = "en kodeutvikler"
-    }
-
-    if (numberForText === 12){
-        textMainSide.textContent = "i skolemodus"
-    }
-}
 }
 
-setInterval(textLoad, 4000)
+setInterval(textLoad, 4000);
 
 textLoad();
 
-
-function downloadURL(URL, fileName){
-    if (isLandscapeMode()) {
+function downloadURL(URL, fileName) {
+  if (isLandscapeMode()) {
     let link = document.createElement("a");
     link.setAttribute("download", fileName);
     link.href = URL;
@@ -89,15 +88,14 @@ function downloadURL(URL, fileName){
     link.click();
     link.remove();
 
-    doneDownloadEl1.innerHTML="Your download is complete";
-    }
-
+    doneDownloadEl1.innerHTML = "Your download is complete";
+  }
 }
 
-let bodyPhoneMyCVDoneEl = document.querySelector(".bodyPhoneMyCVDone")
+let bodyPhoneMyCVDoneEl = document.querySelector(".bodyPhoneMyCVDone");
 
-function downloadURLPhone(URL, fileName){
-    if (isPortraitMode()) {
+function downloadURLPhone(URL, fileName) {
+  if (isPortraitMode()) {
     let link = document.createElement("a");
     link.setAttribute("download", fileName);
     link.href = URL;
@@ -106,15 +104,15 @@ function downloadURLPhone(URL, fileName){
     link.click();
     link.remove();
 
-    bodyPhoneMyCVDoneEl.innerHTML="Your download is complete";
-    }
+    bodyPhoneMyCVDoneEl.innerHTML = "Your download is complete";
+  }
 }
 
 let doneDownloadEl1 = document.querySelector(".doneDownload1");
 let doneDownloadEl2 = document.querySelector(".doneDownload2");
 
-function downloadProjectsURL(URL, fileName){
-    if (isLandscapeMode()) {
+function downloadProjectsURL(URL, fileName) {
+  if (isLandscapeMode()) {
     let link = document.createElement("a");
     link.setAttribute("download", fileName);
     link.href = URL;
@@ -123,30 +121,19 @@ function downloadProjectsURL(URL, fileName){
     link.click();
     link.remove();
 
-    doneDownloadEl2.innerHTML="Your download is complete";
-    }
+    doneDownloadEl2.innerHTML = "Your download is complete";
+  }
 }
 
-document.querySelector(".seeMyCV").addEventListener('click', function () {
-    document.querySelector(".seeThisPart").scrollIntoView({ behavior: 'smooth', });
-}); 
+document.querySelector(".seeMyCV").addEventListener("click", function () {
+  document.querySelector(".seeThisPart").scrollIntoView({ behavior: "smooth" });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init();
+});
 
-
-
-
-
-  document.addEventListener('DOMContentLoaded', function () {
-    AOS.init();
-  });
-
-
-
-
-
-// ?????????
-
-
+// ????????? Youtube tutorial funka ikke? her er linken https://www.youtube.com/watch?v=2IbRtjez6ag
 
 // let cards = document.querySelectorAll(".card")
 
--- Chapter5/portofolio/portefølje.js
+++ Chapter5/portofolio/portefølje.js
@@ -1,5 +1,3 @@
-
-
 let aboutMeButtonEl = document.querySelector(".aboutMeButton");
 let infoAboutMeEl = document.querySelector(".infoAboutMe");
 let myCVButtonEl = document.querySelector(".myCVButton");
@@ -10,8 +8,57 @@ let hoverIfClickEl = document.querySelector(".hoverIfClick");
 let arrowOneImageEl = document.querySelector(".arrowOneImage");
 let arrowTwoImageEl = document.querySelector(".arrowTwoImage");
 let arrowThreeImageEl = document.querySelector(".arrowThreeImage");
-
+let nameAboutMeEl = document.querySelector(".nameAboutMe");
+let interesertAboutMeEl = document.querySelector(".interesertAboutMe");
+let explorerTopText = document.querySelector(".explorerTopText");
+let footerEl = document.querySelector(".footer");
+let interesertAboutMeSideEl = document.querySelector(".interesertAboutMeSide");
+let nameAboutMeSideEl = document.querySelector(".nameAboutMeSide");
+let bodyEl = document.querySelector(".body");
+let bodyPhoneEl = document.querySelector(".bodyPhone");
+let bodyPhoneNameEl = document.querySelector(".bodyPhoneName");
+let bodyPhoneNameH1El = document.querySelector(".bodyPhoneNameH1");
+let bodyPhoneNameP1El = document.querySelector(".bodyPhoneNameP1");
+let bodyPhoneNameP2El = document.querySelector(".bodyPhoneNameP2");
+let bodyPhoneNameImg1El = document.querySelector(".bodyPhoneNameImg1");
+let bodyPhoneNameImg2El = document.querySelector(".bodyPhoneNameImg2");
+let phoneFooterEl = document.querySelector(".phoneFooter");
+let myCVSideEl = document.querySelector(".myCVSide");
+let downloadCVSideEl = document.querySelector(".downloadCVSide");
+let myProjectsSideEl = document.querySelector(".myProjectsSide");
+let downloadProjectsSideEl = document.querySelector(".downloadProjectsSide");
+let myCVAboutMeEl = document.querySelector(".myCVAboutMe");
+let downloadCVAboutMeEl = document.querySelector(".download_CVAboutMe");
+let myProjectsAboutMeEl = document.querySelector(".myProjectsAboutMe");
+let downloadProjectsAboutMeEl = document.querySelector(
+  ".download_ProjectsAboutMe"
+);
+let goBackArrowEl = document.querySelector(".goBackArrow");
 let startMainSideEl = document.querySelector(".startMainSide");
+let bodyPhoneInteresertEl = document.querySelector(".bodyPhoneInteresert");
+let bodyPhoneMyCVEl = document.querySelector(".bodyPhoneMyCV");
+let bodyPhoneDownload_CVEl = document.querySelector(".bodyPhoneDownload_CV");
+let bodyPhoneMyProjectsEl = document.querySelector(".bodyPhoneMyProjects");
+let bodyPhoneDownload_ProjectsEl = document.querySelector(
+  ".bodyPhoneDownload_Projects"
+);
+let bodyPhoneMyProjectsH1El = document.querySelector(".bodyPhoneMyProjectsH1");
+let bodyPhoneInteresertH1El = document.querySelector(".bodyPhoneInteresertH1");
+let bodyPhoneInteresertP1El = document.querySelector(".bodyPhoneInteresertP1");
+let bodyPhoneInteresertP2El = document.querySelector(".bodyPhoneInteresertP2");
+let bodyPhoneInteresertImg2El = document.querySelector(
+  ".bodyPhoneInteresertImg2"
+);
+let bodyPhoneInteresertImg3El = document.querySelector(
+  ".bodyPhoneInteresertImg3"
+);
+let bodyPhoneMyCVTextEl = document.querySelector(".bodyPhoneMyCVText");
+let myCVSeeEl = document.querySelector(".myCVSee");
+let seeMyCVEl = document.querySelector(".seeMyCV");
+let bodyPhoneMyCVH1El = document.querySelector(".bodyPhoneMyCVH1");
+let bodyPhoneMyProjectsLinkEl = document.querySelector(
+  ".bodyPhoneMyProjectsLink"
+);
 
 let aboutMeToggled = false;
 let myCVToggled = false;
@@ -26,7 +73,7 @@ myProjectsButtonEl.addEventListener("click", toggleMyProjects);
 myProjectsButtonEl.addEventListener("touchstart", toggleMyProjects);
 
 function toggleAboutMe() {
-  console.log("test12121")
+  console.log("test12121");
   if (!aboutMeToggled) {
     infoAboutMeEl.classList.add("shown");
     aboutMeToggled = true;
@@ -74,36 +121,8 @@ function reToggleMyProjects() {
   arrowThreeImageEl.style.transform = "rotate(-0deg)";
 }
 
-let nameAboutMeEl = document.querySelector(".nameAboutMe");
-let interesertAboutMeEl = document.querySelector(".interesertAboutMe");
-// let formelAboutMeEl = document.querySelector(".formelAboutMe");
-
-let myCVAboutMeEl = document.querySelector(".myCVAboutMe");
-let downloadCVAboutMeEl = document.querySelector(".download_CVAboutMe");
-
-let myProjectsAboutMeEl = document.querySelector(".myProjectsAboutMe");
-let downloadProjectsAboutMeEl = document.querySelector(
-  ".download_ProjectsAboutMe"
-);
-
-let explorerTopText = document.querySelector(".explorerTopText");
-
-let interesertAboutMeSideEl = document.querySelector(".interesertAboutMeSide");
-// let formelAboutMeSideEl = document.querySelector(".formelAboutMeSide");
-let nameAboutMeSideEl = document.querySelector(".nameAboutMeSide");
-
-let myCVSideEl = document.querySelector(".myCVSide");
-let downloadCVSideEl = document.querySelector(".downloadCVSide");
-
-let myProjectsSideEl = document.querySelector(".myProjectsSide");
-let downloadProjectsSideEl = document.querySelector(
-  ".downloadProjectsSide"
-);
-
-let footerEl = document.querySelector(".footer");
-
-explorerTopText.addEventListener("click", ()=> togglePage("mainSide"));
-explorerTopText.addEventListener("touchstart", ()=> togglePage("mainSide"));
+explorerTopText.addEventListener("click", () => togglePage("mainSide"));
+explorerTopText.addEventListener("touchstart", () => togglePage("mainSide"));
 
 nameAboutMeEl.addEventListener("click", () => togglePage("name"));
 nameAboutMeEl.addEventListener("touchstart", () => togglePage("name"));
@@ -117,7 +136,6 @@ interesertAboutMeEl.addEventListener("touchstart", () => {
 
 myCVAboutMeEl.addEventListener("click", () => togglePage("myCV"));
 myCVAboutMeEl.addEventListener("touchstart", () => togglePage("myCV"));
-
 
 downloadCVAboutMeEl.addEventListener("click", () => {
   togglePage("downloadCV");
@@ -167,7 +185,6 @@ function toggleOffAllPages() {
 
   // footerEl.classList.add("hidden");
   // footerEl.classList.remove("show");
-
 }
 
 function togglePage(page) {
@@ -223,148 +240,97 @@ function togglePage(page) {
   }
 }
 
-
 function isPortraitMode() {
   return window.matchMedia("(orientation: portrait)").matches;
 }
 
-
-
 nameAboutMeEl.addEventListener("click", portraitNamePage);
-
-
-
-
-let bodyEl = document.querySelector(".body");
-let bodyPhoneEl = document.querySelector(".bodyPhone");
-
-let bodyPhoneNameEl = document.querySelector(".bodyPhoneName");
-let bodyPhoneNameH1El = document.querySelector(".bodyPhoneNameH1");
-let bodyPhoneNameP1El = document.querySelector(".bodyPhoneNameP1");
-let bodyPhoneNameP2El = document.querySelector(".bodyPhoneNameP2");
-let bodyPhoneNameImg1El = document.querySelector(".bodyPhoneNameImg1");
-let bodyPhoneNameImg2El = document.querySelector(".bodyPhoneNameImg2");
-let phoneFooterEl = document.querySelector(".phoneFooter");
-
-
-
-let goBackArrowEl = document.querySelector(".goBackArrow");
 goBackArrowEl.addEventListener("click", goBack);
 
-function portraitNamePage(){
-  if (isPortraitMode()){
-  bodyEl.classList.add("hidden");
-  bodyPhoneNameH1El.innerHTML = "Thorstein Andreassen";
-  bodyPhoneNameP1El.innerHTML = "Jeg heter thorstein andreassen og går på lillestrøm vgs. Dette er min mobil versjon, av nettsiden/porteføljen min. Dersom du vil se en bedre versjonen av denne nettsiden. Åpne den på pcen din, der du har media orientation på landscape istedenfor portrait som er automatisk på mobil :DD"
-  bodyPhoneNameP2El.innerHTML = "dette er kun for å vise at jeg kan putte inn ett bilde om jeg vil"
-  console.log(bodyPhoneNameImg2El.classList)
-  bodyPhoneNameImg2El.classList.remove("bodyPhoneNameImg2");
-  goBackArrowEl.style.display = "block";
-}
-}
-
-function goBack(){{
-    if (isPortraitMode()){
-    bodyEl.classList.remove("hidden");
-    bodyPhoneNameH1El.innerHTML = "";
-    bodyPhoneNameP1El.innerHTML = "";
-    bodyPhoneNameP2El.innerHTML = "";
-    bodyPhoneInteresertH1El.innerHTML = "";
-    bodyPhoneInteresertP1El.innerHTML = "";
-    bodyPhoneInteresertP2El.innerHTML = "";
-    bodyPhoneMyCVH1El.innerHTML = "";
-    bodyPhoneMyCVTextEl.innerHTML = "";
-    console.log(bodyPhoneNameImg2El.classList)
-    bodyPhoneNameImg2El.classList.add("bodyPhoneNameImg2");
-    bodyPhoneInteresertImg2El.classList.add("bodyPhoneInteresertImg2");
-    goBackArrowEl.style.display = "none";
-    myCVSideEl.classList.remove("show");
-  }
+function portraitNamePage() {
+  if (isPortraitMode()) {
+    bodyEl.classList.add("hidden");
+    bodyPhoneNameH1El.innerHTML = "Thorstein Andreassen";
+    bodyPhoneNameP1El.innerHTML =
+      "Jeg heter thorstein andreassen og går på lillestrøm vgs. Dette er min mobil versjon, av nettsiden/porteføljen min. Dersom du vil se en bedre versjonen av denne nettsiden. Åpne den på pcen din, der du har media orientation på landscape istedenfor portrait som er automatisk på mobil :DD";
+    bodyPhoneNameP2El.innerHTML =
+      "dette er kun for å vise at jeg kan putte inn ett bilde om jeg vil";
+    console.log(bodyPhoneNameImg2El.classList);
+    bodyPhoneNameImg2El.classList.remove("bodyPhoneNameImg2");
+    goBackArrowEl.style.display = "block";
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-let bodyPhoneInteresertEl = document.querySelector(".bodyPhoneInteresert");
-let bodyPhoneMyCVEl = document.querySelector(".bodyPhoneMyCV");
-let bodyPhoneDownload_CVEl = document.querySelector(".bodyPhoneDownload_CV");
-let bodyPhoneMyProjectsEl = document.querySelector(".bodyPhoneMyProjects");
-let bodyPhoneDownload_ProjectsEl = document.querySelector(".bodyPhoneDownload_Projects");
-let bodyPhoneMyProjectsH1El = document.querySelector(".bodyPhoneMyProjectsH1");
-
-
-
-
-
-
+function goBack() {
+  {
+    if (isPortraitMode()) {
+      bodyEl.classList.remove("hidden");
+      bodyPhoneNameH1El.innerHTML = "";
+      bodyPhoneNameP1El.innerHTML = "";
+      bodyPhoneNameP2El.innerHTML = "";
+      bodyPhoneInteresertH1El.innerHTML = "";
+      bodyPhoneInteresertP1El.innerHTML = "";
+      bodyPhoneInteresertP2El.innerHTML = "";
+      bodyPhoneMyCVH1El.innerHTML = "";
+      bodyPhoneMyCVTextEl.innerHTML = "";
+      console.log(bodyPhoneNameImg2El.classList);
+      bodyPhoneNameImg2El.classList.add("bodyPhoneNameImg2");
+      bodyPhoneInteresertImg2El.classList.add("bodyPhoneInteresertImg2");
+      goBackArrowEl.style.display = "none";
+      myCVSideEl.classList.remove("show");
+    }
+  }
+}
 
 interesertAboutMeEl.addEventListener("click", portraitInteresertPage);
-
-
-
-
-
-
-let bodyPhoneInteresertH1El = document.querySelector(".bodyPhoneInteresertH1");
-let bodyPhoneInteresertP1El = document.querySelector(".bodyPhoneInteresertP1");
-let bodyPhoneInteresertP2El = document.querySelector(".bodyPhoneInteresertP2");
-let bodyPhoneInteresertImg2El = document.querySelector(".bodyPhoneInteresertImg2");
-let bodyPhoneInteresertImg3El = document.querySelector(".bodyPhoneInteresertImg3");
-let bodyPhoneMyCVTextEl = document.querySelector(".bodyPhoneMyCVText");
-let myCVSeeEl = document.querySelector(".myCVSee");
-let seeMyCVEl = document.querySelector(".seeMyCV");
 myProjectsAboutMeEl.addEventListener("click", checkProjects);
-let bodyPhoneMyProjectsLinkEl = document.querySelector(".bodyPhoneMyProjectsLink");
-
-let bodyPhoneMyCVH1El = document.querySelector(".bodyPhoneMyCVH1")
 
 goBackArrowEl.addEventListener("click", goBack);
 
-function portraitInteresertPage(){
-  if (isPortraitMode()){
-  bodyEl.classList.add("hidden");
-  bodyPhoneNameH1El.innerHTML = "Hva jeg er interesert i";
-  bodyPhoneNameP1El.innerHTML = "Jeg synes mye er kult, blant annet it er fett. Jeg liker også å trene, mer spesifikt bryting og styrke"
-  bodyPhoneNameP2El.innerHTML = "Her har dere ett bilde som kan være relevant"
-  console.log(bodyPhoneNameImg2El.classList)
-  bodyPhoneInteresertImg2El.classList.remove("bodyPhoneInteresertImg2");
-  goBackArrowEl.style.display = "block";
-}
+function portraitInteresertPage() {
+  if (isPortraitMode()) {
+    bodyEl.classList.add("hidden");
+    bodyPhoneNameH1El.innerHTML = "Hva jeg er interesert i";
+    bodyPhoneNameP1El.innerHTML =
+      "Jeg synes mye er kult, blant annet it er fett. Jeg liker også å trene, mer spesifikt bryting og styrke";
+    bodyPhoneNameP2El.innerHTML =
+      "Her har dere ett bilde som kan være relevant";
+    console.log(bodyPhoneNameImg2El.classList);
+    bodyPhoneInteresertImg2El.classList.remove("bodyPhoneInteresertImg2");
+    goBackArrowEl.style.display = "block";
+  }
 }
 
-myCVAboutMeEl.addEventListener("click", bodyPhoneCV)
+myCVAboutMeEl.addEventListener("click", bodyPhoneCV);
 
-function bodyPhoneCV(){
-  if (isPortraitMode()){
+function bodyPhoneCV() {
+  if (isPortraitMode()) {
     bodyEl.classList.add("hidden");
     bodyPhoneMyCVH1El.innerHTML = "trykk her for å se min CV";
     goBackArrowEl.style.display = "block";
-}}
+  }
+}
 
 bodyPhoneMyCVEl.addEventListener("click", checkCV);
 
 function checkCV() {
-  if (isPortraitMode()){
-  bodyPhoneMyCVEl.style.display = "none";
-  bodyPhoneMyCVTextEl.innerHTML = "Jeg er en entusiastisk 17-åring med en lidenskap for programmering og en sterk akademisk bakgrunn innen realfag. Min erfaring inkluderer arbeid på en golfbane, hvor jeg utviklet gode teamarbeids- og kommunikasjonsevner. Videre har jeg arbeidet på en gård, hvor jeg har utviklet fysiske ferdigheter og ansvarsbevissthet. Jeg har også drevet med hagearbeid for andre, demonstrert mine organisatoriske evner og kreative tilnærming til oppgaver. Min interesse for teknologi og koding har ført til at jeg har utforsket flere programmeringsspråk og prosjekter på egenhånd. Som en dedikert og lærevillig person, er jeg ivrig etter å utvide mine ferdigheter og bidra til teknologiske løsninger. For ytterligere informasjon om min bakgrunn og prosjekter, oppfordrer jeg deg til å besøke min nettside på pc-versjonen. Jeg ser frem til å diskutere hvordan mine ferdigheter og lidenskaper kan bidra til deres team.";
-  bodyPhoneMyCVH1El.style.display = "none";
-  bodyPhoneMyCVEl.style.display = "block";
-}}
+  if (isPortraitMode()) {
+    bodyPhoneMyCVEl.style.display = "none";
+    bodyPhoneMyCVTextEl.innerHTML =
+      "Jeg er en entusiastisk 17-åring med en lidenskap for programmering og en sterk akademisk bakgrunn innen realfag. Min erfaring inkluderer arbeid på en golfbane, hvor jeg utviklet gode teamarbeids- og kommunikasjonsevner. Videre har jeg arbeidet på en gård, hvor jeg har utviklet fysiske ferdigheter og ansvarsbevissthet. Jeg har også drevet med hagearbeid for andre, demonstrert mine organisatoriske evner og kreative tilnærming til oppgaver. Min interesse for teknologi og koding har ført til at jeg har utforsket flere programmeringsspråk og prosjekter på egenhånd. Som en dedikert og lærevillig person, er jeg ivrig etter å utvide mine ferdigheter og bidra til teknologiske løsninger. For ytterligere informasjon om min bakgrunn og prosjekter, oppfordrer jeg deg til å besøke min nettside på pc-versjonen. Jeg ser frem til å diskutere hvordan mine ferdigheter og lidenskaper kan bidra til deres team.";
+    bodyPhoneMyCVH1El.style.display = "none";
+    bodyPhoneMyCVEl.style.display = "block";
+  }
+}
 
 function checkProjects() {
-  if (isPortraitMode()){
-  bodyEl.classList.add("hidden");
-  bodyPhoneMyProjectsLinkEl.style.display = "block";
-  goBackArrowEl.style.display = "block";
-  phoneFooterEl.classList.add("projectsPhoneFooter");
-  phoneFooterEl.classList.remove("phoneFooter")
-  bodyPhoneMyProjectsH1El.innerHTML = "under ser du mine prosjekter, for å kunne bruke de ordentlig må du være på pc. Grunnet at jeg hadde disse prosjektene før vi lærte om mobil modus"
-}}
+  if (isPortraitMode()) {
+    bodyEl.classList.add("hidden");
+    bodyPhoneMyProjectsLinkEl.style.display = "block";
+    goBackArrowEl.style.display = "block";
+    phoneFooterEl.classList.add("projectsPhoneFooter");
+    phoneFooterEl.classList.remove("phoneFooter");
+    bodyPhoneMyProjectsH1El.innerHTML =
+      "under ser du mine prosjekter, for å kunne bruke de ordentlig må du være på pc. Grunnet at jeg hadde disse prosjektene før vi lærte om mobil modus";
+  }
+}
